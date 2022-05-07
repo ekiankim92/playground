3cf4764a108cd9da5fb10fe60cbc0b6de988a946
ekim <eunkukkim1992@gmail.com>
2022-05-07 22:27:28 +0900

    update

--- pages/input/index.tsx
+++ pages/input/index.tsx
@@ -16,10 +16,48 @@ const InputPage = () => {
     console.log("event.target.value:", event.target.value);
   };
 
+  const person = [
+    {
+      name: "Ian",
+      age: 31,
+      gender: "male",
+    },
+    {
+      name: "Grace",
+      age: 28,
+      gender: "female",
+    },
+
+    // person1: {
+    //   name: "Ian",
+    //   age: 31,
+    //   gender: "male",
+    // },
+    // person2: {
+    //   name: "Grace",
+    //   age: 28,
+    //   gender: "female",
+    // },
+    // person3: "Jessica",
+    // person4: "Jack",
+    // person5: "Hillary",
+    // person6: "Sam",
+  ];
+
+  console.log(person);
+
   return (
     <>
       <div>Hello WOrld</div>
       <Input type="text" onChange={onChangeName} />
+      {person.map((el) => (
+        <div key={el}>
+          <div>{el.name}</div>
+          {/* <h1>{el.person1.name}</h1> */}
+          {/* <h1>{el.person2.name}</h1> */}
+        </div>
+      ))}
+      {/* <div>{person[0].person1}</div> */}
     </>
   );
 };
