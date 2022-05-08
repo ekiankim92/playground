434137553e5dd45eadc3dbc5379c6a55e6056df0
ekim <eunkukkim1992@gmail.com>
2022-05-08 11:16:55 +0900

    update

--- pages/input/index.tsx
+++ pages/input/index.tsx
@@ -17,34 +17,47 @@ const InputPage = () => {
   };
 
   const person = [
-    {
-      name: "Ian",
-      age: 31,
-      gender: "male",
-    },
-    {
-      name: "Grace",
-      age: 28,
-      gender: "female",
-    },
-
-    // person1: {
+    // {
     //   name: "Ian",
     //   age: 31,
     //   gender: "male",
     // },
-    // person2: {
+    // {
     //   name: "Grace",
     //   age: 28,
     //   gender: "female",
     // },
-    // person3: "Jessica",
-    // person4: "Jack",
-    // person5: "Hillary",
-    // person6: "Sam",
+    {
+      person1: {
+        name: "Ian",
+        age: 31,
+        gender: "male",
+      },
+      person2: {
+        name: "Grace",
+        age: 28,
+        gender: "female",
+      },
+      person3: "Nani",
+      person4: "Jack",
+      person5: "Hillary",
+      person6: "Sam",
+    },
   ];
 
   console.log(person);
+
+  const keys = Object.keys(person[0]);
+
+  console.log("keys:", keys);
+
+  const values = Object.values(person[0]);
+
+  console.log("values:", values);
+
+  const { choice1, choice2, choice3 } = person;
+
+  console.log("choice1:", choice1);
 
   return (
     <>
@@ -52,9 +65,11 @@ const InputPage = () => {
       <Input type="text" onChange={onChangeName} />
       {person.map((el) => (
         <div key={el}>
-          <div>{el.name}</div>
-          {/* <h1>{el.person1.name}</h1> */}
-          {/* <h1>{el.person2.name}</h1> */}
+          {/* <div>{el.name}</div> */}
+          <h1>{el.person1.name}</h1>
+          {/* <div>{el.person1}</div> */}
+          <h1>{el.person2.name}</h1>
+          <h1>{el.person3}</h1>
         </div>
       ))}
       {/* <div>{person[0].person1}</div> */}
