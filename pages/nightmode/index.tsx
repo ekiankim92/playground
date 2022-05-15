21043280b44462325695a488ce269a209e454f14
ekim <eunkukkim1992@gmail.com>
2022-05-15 14:52:21 +0900

    update

---
+++ pages/nightmode/index.tsx
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+import { Switch } from "antd";
+import "antd/dist/antd.css";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+  width: 600px;
+  height: 600px;
+  border: 1px solid #000;
+  margin: 60px auto;
+  background-color: ${(props) => (props.isNight ? "#fff" : "#000")};
+`;
+
+const Title = styled.h3`
+  margin: 12px;
+`;
+
+const ToggleWrapper = styled.div`
+  width: 340px;
+  text-align: end;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ParagraphWrapper = styled.div`
+  margin-top: 20px;
+  padding: 20px;
+  line-height: 32px;
+`;
+
+const NightMode = () => {
+  const [isNight, setIsNight] = useState(false);
+
+  const onChangeToggle = (checked) => {
+    console.log(`switch to ${checked}`);
+    setIsNight((prev) => !prev);
+  };
+
+  return (
+    <Wrapper isNight={isNight}>
+      <TitleWrapper>
+        <Title>Night/Day Mode Toggle</Title>
+        <ToggleWrapper>
+          <Switch defaultChecked onChange={onChangeToggle} />
+        </ToggleWrapper>
+      </TitleWrapper>
+      <ParagraphWrapper>
+        <p>
+          Click on the moon in the upper-right corner to change to night mode.
+          Click again o the sun to change back to day mode.
+        </p>
+      </ParagraphWrapper>
+    </Wrapper>
+  );
+};
+export default NightMode;
