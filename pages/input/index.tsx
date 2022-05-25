54daaa6512be1d7d0bb0bd8d0fbf6918dfe85f30
ekim <eunkukkim1992@gmail.com>
2022-05-26 00:49:42 +0900

    onKeyPress

--- pages/input/index.tsx
+++ pages/input/index.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Input = styled.input`
   width: 260px;
@@ -7,13 +7,53 @@ const Input = styled.input`
   margin: 50px;
 `;
 
+const AddButton = styled.button`
+  width: 200px;
+  height: 40px;
+  margin: -46px;
+`;
+
+const DeleteButton = styled.button`
+  width: 200px;
+  height: 40px;
+  margin: 50px;
+`;
+
 const InputPage = () => {
   const [name, setName] = useState("");
+  const [add, setAdd] = useState("");
+  const [addList, setAddList] = useState([]);
+  const clearRef = useRef(null);
 
   const onChangeName = (event) => {
     setName(event.target.value);
     console.log("name:", name);
     console.log("event.target.value:", event.target.value);
+  };
+
+  const onChangeAddName = (event) => {
+    setAdd(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const onClickAddNames = () => {
+    clearRef.current.value = "";
+    setAddList([...addList, add]);
+    addList.join("");
+    console.log("addList:", addList);
+  };
+
+  const onClickDelete = () => {
+    setAddList([]);
+    console.log("addList:", addList);
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setAddList([...addList, add]);
+      clearRef.current.value = "";
+    }
+    console.log("event:", event);
   };
 
   const person = [
@@ -70,9 +110,19 @@ const InputPage = () => {
           {/* <div>{el.person1}</div> */}
           <h1>{el.person2.name}</h1>
           <h1>{el.person3}</h1>
+          <h1>{el.person4}</h1>
         </div>
       ))}
-      {/* <div>{person[0].person1}</div> */}
+      <Input
+        placeholder="add names to the list"
+        onChange={onChangeAddName}
+        ref={clearRef}
+        onKeyPress={onKeyPress}
+      />
+      <AddButton onClick={onClickAddNames}>Add</AddButton>
+      <DeleteButton onClick={onClickDelete}>Clear All</DeleteButton>
+      <div>Added List</div>
+      <div>{addList}</div>
     </>
   );
 };
