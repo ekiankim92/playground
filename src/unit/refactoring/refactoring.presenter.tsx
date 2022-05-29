159c36607bc31704223082f27d3e87fc6fa923cc
ekim <eunkukkim1992@gmail.com>
2022-05-29 17:59:58 +0900

    refactoring

--- src/unit/refactoring/refactoring.presenter.tsx
+++ src/unit/refactoring/refactoring.presenter.tsx
@@ -4,6 +4,25 @@ export default function RefactoringUI() {
   return (
     <S.Wrapper>
       <S.Title>Register</S.Title>
+      <S.MainWrapper>
+        <S.EmailWrapper>
+          <label>Email</label>
+          <input type="email" />
+        </S.EmailWrapper>
+        <S.NameWrapper>
+          <label>Name</label>
+          <input type="text" />
+        </S.NameWrapper>
+        <S.PasswordWrapper>
+          <label>Password</label>
+          <input type="password" />
+        </S.PasswordWrapper>
+        <S.ConfirmPasswordWrapper>
+          <label>Confirm Password</label>
+          <input type="password" />
+        </S.ConfirmPasswordWrapper>
+        <button>Submit</button>
+      </S.MainWrapper>
     </S.Wrapper>
   );
 }
--- src/unit/refactoring/refactoring.styles.ts
+++ src/unit/refactoring/refactoring.styles.ts
@@ -1,7 +1,8 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  width: 1000px;
+  width: 800px;
+  height: 600px;
   border: 1px solid #000;
   margin: 20px auto;
 `;
@@ -9,4 +10,28 @@ export const Wrapper = styled.div`
 export const Title = styled.h2`
   padding: 6px;
   text-align: center;
+`;
+
+export const MainWrapper = styled.section`
+  width: 580px;
+  height: 440px;
+  border: 1px solid #000;
+  margin: 0px auto;
+  box-shadow: 8px 8px 4px 4px;
+`;
+
+export const EmailWrapper = styled.div`
+  background-color: lightcoral;
+`;
+
+export const NameWrapper = styled.div`
+  background-color: lightblue;
+`;
+
+export const PasswordWrapper = styled.div`
+  background-color: lightgreen;
+`;
+
+export const ConfirmPasswordWrapper = styled.div`
+  background-color: beige;
 `;
