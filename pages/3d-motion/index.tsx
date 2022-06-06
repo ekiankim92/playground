5fac24b443f7bffb66c05b8e26f9c1bf943132ac
ekim <eunkukkim1992@gmail.com>
2022-06-06 11:05:29 +0900

    3D Model

---
+++ pages/3d-motion/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styled from "@emotion/styled";
+
+export default function ThreeDimension() {
+  return (
+    <>
+      <h2>3D Motion Page Practice</h2>
+      <Head>
+        <meta charSet="UTF-8" />
+      </Head>
+    </>
+  );
+}
---
+++ pages/doggo/index.js
@@ -0,0 +1,10 @@
+export default function TestingPgae() {
+  const perf = require("./perf.html");
+
+  return (
+    <>
+      <h3>This is the 3D Model Practice</h3>
+      <iframe src={perf}></iframe>
+    </>
+  );
+}
