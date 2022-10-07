2f5811eab0f52641da2fe5c517f9005beb7e3849
ekim <eunkukkim1992@gmail.com>
2022-10-07 10:10:18 +0900

    diary css

--- src/unit/diary/diary.presenter.tsx
+++ src/unit/diary/diary.presenter.tsx
@@ -4,14 +4,14 @@ export default function DiaryUI(props) {
   return (
     <S.Wrapper>
       <S.Title>Today's Diary</S.Title>
-      <input
+      <S.Input
         name="author"
         value={props.author}
         onChange={(event) => {
           props.setAuthor(event.target.value);
         }}
       />
-      <textarea />
+      <S.Text />
       <select>
         <option value={1}>1</option>
         <option value={2}>2</option>
--- src/unit/diary/diary.styles.ts
+++ src/unit/diary/diary.styles.ts
@@ -8,3 +8,15 @@ export const Wrapper = styled.div`
 export const Title = styled.h2`
   padding: 8px;
 `;
+
+export const Input = styled.input`
+  width: 220px;
+  height: 40px;
+  padding: 8px;
+  font-size: 18px;
+`;
+
+export const Text = styled.textarea`
+  width: 400px;
+  height: 200px;
+`;
