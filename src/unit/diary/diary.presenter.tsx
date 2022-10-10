1688d76ae82137411ebc6c241c6c997638564b92
ekim <eunkukkim1992@gmail.com>
2022-10-10 23:54:23 +0900

    diary option value 7

--- src/unit/diary/diary.presenter.tsx
+++ src/unit/diary/diary.presenter.tsx
@@ -19,6 +19,7 @@ export default function DiaryUI(props) {
         <option value={4}>4</option>
         <option value={5}>5</option>
         <option value={6}>6</option>
+        <option value={7}>7</option>
       </select>
     </S.Wrapper>
   );
