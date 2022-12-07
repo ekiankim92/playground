faba91204e0cc60f61386df45c07c83aad9dc59a
ekim <eunkukkim1992@gmail.com>
2022-12-07 10:29:15 +0900

    antd collapse

---
+++ pages/accordion/index.tsx
@@ -0,0 +1,29 @@
+import { Collapse } from "antd";
+import "antd/dist/antd.css";
+
+export default function Accordion() {
+  const { Panel } = Collapse;
+
+  const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+  return (
+    <>
+      <div>Antd Accordion</div>
+      <Collapse accordion expandIconPosition="right" style={{ width: 300 }}>
+        <Panel header="This is panel header 1" key="1">
+          <p>{text}</p>
+        </Panel>
+        <Panel header="This is panel header 2" key="2">
+          <p>{text}</p>
+        </Panel>
+        <Panel header="This is panel header 3" key="3">
+          <p>{text}</p>
+        </Panel>
+      </Collapse>
+    </>
+  );
+}
