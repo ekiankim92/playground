ee426f13bb414b44810f5bad2c1033cb74f7fe9c
ekim <eunkukkim1992@gmail.com>
2022-12-14 16:21:34 +0900

    emailJS

---
+++ pages/emailJS/index.tsx
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("buxi-bux", "buxi_bux", form.current, "CyhTZAOmFAYYekB04")
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log(result);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+export default ContactUs;
