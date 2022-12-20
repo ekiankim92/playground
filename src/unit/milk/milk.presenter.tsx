7817866d687318033c61c74bcc6c2f548bb06416
ekim <eunkukkim1992@gmail.com>
2022-12-20 14:27:31 +0900

    milk animations

--- src/unit/milk/milk.presenter.tsx
+++ src/unit/milk/milk.presenter.tsx
@@ -4,9 +4,41 @@ export default function MilkUI() {
   return (
     <S.Wrapper>
       <S.BackgroundImageWrapper>
+        <S.LeftWrapper>
+          <S.CarWrapper>
+            <S.CarImg src="milk/car.png" />
+          </S.CarWrapper>
+          <S.CarLine />
+          <S.CarArrow />
+          <S.CarB>
+            <S.B>B</S.B>
+          </S.CarB>
+          <S.PlaneWrapper>
+            <S.PlaneImg src="milk/plane.png" />
+          </S.PlaneWrapper>
+          <S.PlaneLine />
+          <S.PlaneArrow />
+          <S.PlantB>
+            <S.B>B</S.B>
+          </S.PlantB>
+          <S.SnorkelWrapper>
+            <S.SnorkelImg src="milk/snorkel.png" />
+          </S.SnorkelWrapper>
+        </S.LeftWrapper>
         <S.PhoneImg src="milk/phone.png" />
         <S.ScreenImg src="milk/screen.png" />
         <S.LogoImg src="milk/buxi.png" />
+        <S.RightWrapper>
+          <S.PresentsWrapper>
+            <S.PresentsImg src="milk/presents.png" />
+          </S.PresentsWrapper>
+          <S.HotelWrapper>
+            <S.HotelImg src="milk/hotel.png" />
+          </S.HotelWrapper>
+          <S.CutleryWrapper>
+            <S.CutleryImg src="milk/cutlery.png" />
+          </S.CutleryWrapper>
+        </S.RightWrapper>
       </S.BackgroundImageWrapper>
     </S.Wrapper>
   );
--- src/unit/milk/milk.styles.ts
+++ src/unit/milk/milk.styles.ts
@@ -1,4 +1,27 @@
 import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
+
+const SlideCar = keyframes`
+    from {
+        transform: translatex(0px) translatey(0px);
+        opacity: 0;
+    }
+    to {
+        transform: translatex(65px) translatey(56px);
+        opacity: 1;
+    }
+`;
+
+const SlidePlane = keyframes`
+    from {
+        left: 90px;
+        opacity: 0;
+    }
+    to {
+        left: 180px;
+        opacity: 1;
+    }
+`;
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -19,7 +42,6 @@ export const BackgroundImageWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  /* position: absolute; */
 `;
 
 export const PhoneImg = styled.img`
@@ -34,4 +56,200 @@ export const ScreenImg = styled.img`
 export const LogoImg = styled.img`
   position: absolute;
   z-index: 1;
+`;
+
+export const LeftWrapper = styled.div`
+  position: relative;
+  width: 320px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const CarWrapper = styled.div`
+  width: 82px;
+  height: 82px;
+  background: #e9f6fb;
+  box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 80px;
+`;
+
+export const CarImg = styled.img`
+  width: 54px;
+  height: 54px;
+`;
+
+export const CarLine = styled.div`
+  width: 88px;
+  border: 1px dashed #9696cf;
+  position: absolute;
+  right: 94px;
+  top: 100px;
+  transform: rotate(40deg);
+`;
+
+export const CarArrow = styled.div`
+  position: absolute;
+  color: #fff;
+  width: 0px;
+  height: 0px;
+  border: 12px solid #9696cf;
+  border-color: transparent transparent transparent #9696cf;
+  transform: rotate(40deg);
+  right: 88px;
+  top: 120px;
+`;
+
+export const CarB = styled.div`
+  position: absolute;
+  width: 44px;
+  height: 44px;
+  background: #9696cf;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  right: 148px;
+  top: 50px;
+  animation: ${SlideCar} 5s infinite;
+`;
+
+export const B = styled.div`
+  font-weight: 700;
+  font-size: 26px;
+`;
+
+export const PlaneWrapper = styled.div`
+  width: 114px;
+  height: 114px;
+  background: #e9f6fb;
+  box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PlaneImg = styled.img`
+  width: 66px;
+  height: 66px;
+`;
+
+export const PlaneLine = styled.div`
+  width: 88px;
+  border: 1px dashed #9696cf;
+  position: absolute;
+  top: 204px;
+  right: 118px;
+`;
+
+export const PlaneArrow = styled.div`
+  position: absolute;
+  color: #fff;
+  width: 0px;
+  height: 0px;
+  border: 12px solid #9696cf;
+  border-color: transparent transparent transparent #9696cf;
+  top: 193px;
+  right: 100px;
+`;
+
+export const PlantB = styled.div`
+  position: absolute;
+  width: 44px;
+  height: 44px;
+  background: #add8e6;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 183px;
+  right: 180px;
+  animation: ${SlidePlane} 3s infinite;
+`;
+
+export const SnorkelWrapper = styled.div`
+  width: 73px;
+  height: 73px;
+  background: #e9f6fb;
+  box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 80px;
+`;
+
+export const SnorkelImg = styled.img`
+  width: 47px;
+  height: 47px;
+`;
+
+export const RightWrapper = styled.div`
+  width: 320px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+export const PresentsWrapper = styled.div`
+  width: 112px;
+  height: 112px;
+  background: #e9f6fb;
+  box-shadow: -4px 4px 6px rgba(0, 0, 0, 0.1);
+  transform: matrix(-1, 0, 0, 1, 0, 0);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 80px;
+`;
+
+export const PresentsImg = styled.img`
+  width: 72px;
+  height: 72px;
+`;
+
+export const HotelWrapper = styled.div`
+  width: 66px;
+  height: 66px;
+  background: #e9f6fb;
+  box-shadow: -4px 4px 6px rgba(0, 0, 0, 0.1);
+  transform: matrix(-1, 0, 0, 1, 0, 0);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 40px;
+`;
+
+export const HotelImg = styled.img`
+  width: 38px;
+  height: 38px;
+`;
+
+export const CutleryWrapper = styled.div`
+  width: 82px;
+  height: 82px;
+  background: #e9f6fb;
+  box-shadow: -4px 4px 6px rgba(0, 0, 0, 0.1);
+  transform: matrix(-1, 0, 0, 1, 0, 0);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 80px;
+`;
+
+export const CutleryImg = styled.img`
+  width: 45px;
+  height: 45px;
 `;
