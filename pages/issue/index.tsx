cd087058ef093789f97fe5b0d0925196b5ec1671
ekim <eunkukkim1992@gmail.com>
2023-02-02 23:19:45 +0900

    connecting PR with issue

--- pages/issue/index.tsx
+++ pages/issue/index.tsx
@@ -3,6 +3,9 @@ export default function Issue01() {
     <>
       <div>This is the first issue testing!</div>
       <div>This is the second issue testing!</div>
+      <div>
+        This is adding the last and final issue connecting PR with issue
+      </div>
     </>
   );
 }
