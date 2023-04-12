8e9eda0e25d51ab9a997f35dc7027cefc2049fa5
ekim <eunkukkim1992@gmail.com>
2023-04-12 16:15:10 +0900

    highcharts zoomType

--- pages/highcharts/cdn.tsx
+++ pages/highcharts/cdn.tsx
@@ -1,35 +1,33 @@
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import Head from "next/head";
+import styled from "@emotion/styled";
 
 export default function Overflow() {
   const options = {
     chart: {
-      type: "column",
+      // type: "column",
+      type: "line",
+      zoomType: "x",
       backgroundColor: "transparent",
+    },
+    xAxis: {
+      // min: 2001,
+      // max: 5,
+      categories: [
+        2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
+        2013, 2014,
+      ],
+      scrollbar: {
+        enabled: true,
+      },
     },
     series: [
       {
-        data: [3, 2, 4, 6],
-        color: "red",
-      },
-      {
+        name: "Tokyo",
         data: [
-          {
-            y: 3,
-            color: {
-              pattern: {
-                path: {
-                  stroke: "#000000",
-                  "stroke-width": 2,
-                  rotation: 45,
-                  d: ["M", 10, 10, "L", 30, 30, "z"],
-                },
-                width: 40,
-                height: 40,
-              },
-            },
-          },
+          7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,
+          30, 32.4, 36.6, 40, 42.5,
         ],
       },
     ],
@@ -37,9 +35,23 @@ export default function Overflow() {
   return (
     <>
       <Head>
-        <script src="https://code.highcharts.com/modules/pattern-fill.js"></script>
+        <script
+          type="text/javascript"
+          src="http://code.highcharts.com/stock/highstock.js"
+        ></script>
+        <script src="http://code.highcharts.com/modules/exporting.js"></script>
       </Head>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <Wrapper>
+        <HighchartsReact highcharts={Highcharts} options={options} />
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  width: 600px;
+  height: 600px;
+  overflow: scroll;
+  margin: 40px auto;
+  border: 1px solid #000;
+`;
